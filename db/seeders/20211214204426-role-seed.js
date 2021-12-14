a9923aa8ab62e01caaf30e909c89cdc659df5602
cat-friend <denisepli@gmail.com>
2021-12-14 16:37:12 -0500

    completed baseline seed data for Roles, TagCategories, StatusTypes, Tags, UserRoles DPL

--- db/seeders/20211214204426-role-seed.js
+++ db/seeders/20211214204426-role-seed.js
@@ -7,20 +7,20 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+
     */
+      return queryInterface.bulkInsert('Roles', [
+        {name: 'admin', createdAt: new Date(), updatedAt: new Date()},
+        {name: 'normal', createdAt: new Date(), updatedAt: new Date()}
+      ], {});
   },
 
   down: (queryInterface, Sequelize) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
-
       Example:
-      return queryInterface.bulkDelete('People', null, {});
     */
+      return queryInterface.bulkDelete('Roles', null, {});
   }
 };
--- db/seeders/20211214204430-user-role-seed.js
+++ db/seeders/20211214204430-user-role-seed.js
@@ -5,13 +5,11 @@ module.exports = {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
     */
+      return queryInterface.bulkInsert('UserRoles', [
+        {userId: 1, roleId: 2, createdAt: new Date(), updatedAt: new Date()},
+        {userId: 2, roleId: 2, createdAt: new Date(), updatedAt: new Date()},
+      ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +18,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
     */
+      return queryInterface.bulkDelete('UserRoles', null, {});
   }
 };
