5a0f378d27d6aa81c3d418775730b761cdf0d676
aletheia <aletheiak93@gmail.com>
2021-12-15 10:50:56 -0800

    added recipes to be accessible in home route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { asyncHandler } = require('./utils');
+const db = require('../db/models');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', asyncHandler(async function (req, res, next) {
+  const recipes = await db.Recipe.findAll(
+    {
+      limit: 9,
+      include: [
+        {
+          model: db.Tag
+        },
+        {
+          model: db.StatusType
+        },
+        {
+          model: db.Image
+        },
+        {
+          model: db.Review
+        }
+      ]
+    }
+  )
+
+  res.render('index', { title: 'a/A Express Skeleton Home' }, recipes);
+}));
 
 module.exports = router;
