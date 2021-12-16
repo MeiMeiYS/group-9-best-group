54e09236e714e0bd1f9a66e6b1278ff34889a973
aletheia <aletheiak93@gmail.com>
2021-12-16 12:51:20 -0800

    deleted extra unneeded id variables

--- routes/users.js
+++ routes/users.js
@@ -209,7 +209,7 @@ router.get('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
 router.get('/:id(\\d+)/edit', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
   const userId = parseInt(req.params.id);
   const user = await User.findByPk(userId);
-  checkPermissionsUsersRoute(user, res.locals.user, id);
+  checkPermissionsUsersRoute(user, res.locals.user);
 
   res.render('users-edit', { title: 'Edit User', user, csrfToken: req.csrfToken() })
 
@@ -239,7 +239,7 @@ router.post('/:id(\\d+)/image/new', requireAuth, csrfProtection, imageValidators
   const userId = parseInt(req.params.id);
 
   const user = await User.findByPk(userId);
-  checkPermissionsUsersRoute(user, res.locals.user, id);
+  checkPermissionsUsersRoute(user, res.locals.user);
 
   const validatorErrors = validationResult(req);
 
