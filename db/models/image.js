7c4a4abe8fd7b9e4642240db6955b62f8da37411
meimei <shihmeiyin@gmail.com>
2021-12-17 12:36:56 -0800

    Raise url input length from 255 to 600 in modle and migration files, create frontend js validation on create recipe from

--- db/models/image.js
+++ db/models/image.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Image = sequelize.define('Image', {
-    url: DataTypes.STRING
+    url: DataTypes.STRING(600)
   }, {});
   Image.associate = function(models) {
     // associations can be defined here
