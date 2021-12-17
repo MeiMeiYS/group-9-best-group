76552bdc3d32092d13391fc4302aaf09710f9611
cat-friend <denisepli@gmail.com>
2021-12-16 20:35:08 -0500

    added event listeners for Add a Review  and Cancel buttons

--- public/javascripts/api-reviews.js
+++ public/javascripts/api-reviews.js
@@ -1,9 +1,44 @@
+// const router = require('../../routes/api/reviews');
+
+// building `Add A Review` form
+const reviewFormDiv = document.createElement("div");
 const reviewForm = document.createElement("form");
 const reviewText = document.createElement("textarea");
+const imageURL = document.createElement("input");
+imageURL.setAttribute("type", "url");
+imageURL.setAttribute("name", "imageURL");
+const imageURLLabel = document.createElement("label");
+imageURLLabel.htmlFor = "imageURL";
+imageURLLabel.innerText = `URL for an Image (Optional)`;
+console.log(imageURL);
+reviewForm.appendChild(reviewText);
 reviewText.style.width = `100%`;
 reviewText.style.height = `80%`;
-const reviewButtons = document.createElement("a");
+reviewText.setAttribute("name", "review");
+const reviewButtonsDiv = document.createElement("div");
+const submitButton = document.createElement("a");
+const cancelButton = document.createElement("a");
+(() => {
+    const buttons = [submitButton, cancelButton];
+    const formDivEles = [reviewForm, imageURLLabel, imageURL, reviewButtonsDiv]
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute("href", "#")
+        reviewButtonsDiv.appendChild(buttons[i]);
+    }
+    for (let i = 0; i < formDivEles.length; i++) {
+        formDivEles[i].setAttribute("href", "#")
+        reviewFormDiv.appendChild(formDivEles[i]);
+    }
+})();
+reviewFormDiv.appendChild(reviewButtonsDiv);
+submitButton.innerText = "SUBMIT"
+cancelButton.innerText = "CANCEL"
+// when cancel button is pressed, form goes away, submitButton comes back
 
+// when submit button is clicked, need to invoke newReview();
+// event listener for submit button
+// --> submit button only appears when Submit a New Recipe is clicked
+// --> when reviewForm is made, make a new
 
 
 document.addEventListener("DOMContentLoaded", event => {
@@ -11,13 +46,39 @@ document.addEventListener("DOMContentLoaded", event => {
     // if (res.locals.user) {
     //     currUser.id = res.locals.user.id;
     // }
-
-
-    // add a review button
+    // `Add a Review` button
     const addAReview = document.getElementById('addAReview');
     addAReview.addEventListener("click", event => {
         event.stopPropagation();
-        addAReview.innerHTML = "<h1>Henlo?</h1>"
+        addAReview.replaceWith(reviewFormDiv);
+    });
+    //`Submit` button
+    submitButton.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const body = {
+            userId: 1, //this will be res.locals.user
+            review: reviewText.value,
+            imageURL: imageURL.value
+        }
+        newReview(body)
     });
 
+    // `Cancel` Button
+    cancelButton.addEventListener("click", (event) => {
+        event.stopPropagation();
+        reviewFormDiv.replaceWith(addAReview);
+    })
 });
+
+// posting a new review function
+async function newReview(bodyJS) {
+    const body = JSON.stringify(bodyJS);
+    console.log("body");
+    await fetch("/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: body
+    })
+};
